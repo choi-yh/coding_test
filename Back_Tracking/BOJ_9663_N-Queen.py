70b88512e00b0de1d010734cec9b502864b6034e
younghyo_desktop <yhchoi0102@gmail.com>
2021-09-23 21:33:43 +0900

    [BOJ] 9663 back tracking

---
+++ Back_Tracking/BOJ_9663_N-Queen.py
@@ -0,0 +1,29 @@
+# https://www.acmicpc.net/problem/9663
+import sys
+
+n = int(sys.stdin.readline())
+answer = 0
+board = [0] * n
+
+
+def check(row):
+    for i in range(row):
+        if board[row] == board[i] or (abs(board[row] - board[i]) == abs(row - i)):
+            return False
+    return True
+
+
+def queen(row):
+    global answer
+
+    if row == n:
+        answer += 1
+    else:
+        for i in range(n):
+            board[row] = i
+            if check(row):
+                queen(row + 1)
+
+
+queen(0)
+print(answer)
